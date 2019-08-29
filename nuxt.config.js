6557c53d3fc7b58083a41d4e59314941101376ca
infiniteSky <boxlab@foxmail.com>
2019-08-29 20:24:10 +0800

    修改移动端Chrome地址栏颜色

--- nuxt.config.js
+++ nuxt.config.js
@@ -8,7 +8,8 @@ export default {
     meta: [
       {charset: 'utf-8'},
       {name: 'viewport', content: 'width=device-width, initial-scale=1'},
-      {hid: 'description', name: 'description', content: process.env.npm_package_description || ''}
+      {hid: 'description', name: 'description', content: process.env.npm_package_description || ''},
+      {name: 'theme-color', content: '#393939'},
     ],
     link: [
       {rel: 'icon', type: 'image/x-icon', href: '/favicon.ico'}
